b6d40297a4b93d5ff2018f900567e8fe7ce4fe72
SoniaPria <webusable@protonmail.com>
2023-01-31 19:22:16 +0100

    Creada clase Question

---
+++ Assets/Scripts/QuestionCs.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionCs : MonoBehaviour
+{
+    public string category;
+    public string type;
+    public string difficulty;
+    public string question;
+    public string correct_answer;
+    public string[] incorrect_answers;
+}
