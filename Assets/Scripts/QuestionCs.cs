73ad376d99d3741cb3477117e962afb0ec71a9bb
SoniaPria <webusable@protonmail.com>
2023-02-08 15:21:22 +0100

    Cambia class QuestionCS a serializable

--- Assets/Scripts/QuestionCs.cs
+++ Assets/Scripts/QuestionCs.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
-public class QuestionCs : MonoBehaviour
+[Serializable]
+public class QuestionCs
 {
     public string category;
     public string type;
