be9678b3691ddecd4c4556bcd7f44262f95205de
SoniaPria <webusable@protonmail.com>
2023-02-08 16:30:49 +0100

    Imprime o nº de preguntas solicitadas e 4 datos da 1ª pregunta

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,17 +9,16 @@ public class GameManager : MonoBehaviour
    string api = "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=boolean";
 
    TrivialCs getResults;
+
+   // Crea a variable cos datos como un atributo público do script principal
    public QuestionCs getQuestion;
 
+   // Plantilla extraída de
+   // Unity Docs | UnityWebRequest.Get
     void Awake()
     {
         // A correct website page.
         StartCoroutine( GetRequest( api ) );
-    }
-
-    void Start()
-    {
-
     }
 
     IEnumerator GetRequest( string uri )
@@ -42,7 +41,7 @@ public class GameManager : MonoBehaviour
                     Debug.LogError(pages[page] + ": HTTP Error: " + wr.error);
                 break;
                 case UnityWebRequest.Result.Success:
-                    Debug.Log(pages[page] + ":\nReceived: " + wr.downloadHandler.text);
+                    // Debug.Log(pages[page] + ":\nReceived: " + wr.downloadHandler.text);
 
                     CreateFromJSON( wr.downloadHandler.text );
                 break;
@@ -50,9 +49,28 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void CreateFromJSON(string jsonString)
+    // JsonUtility.FromJson
+    void CreateFromJSON( string jsonString )
     {
         getResults = JsonUtility.FromJson<TrivialCs>(jsonString);
         getQuestion = getResults.results[0];
+
+        PrintGetQuestion();
+    }
+
+    void PrintGetQuestion()
+    {
+        // Imprime na consola o número de preguntas solicitadas
+        // e 4 datos da primeira pregunta
+
+        int nqq = getResults.results.Count;
+
+        print("O número de preguntas solicitadas é: " + nqq );
+
+        print("Pregunta 1");
+        print("Categoría " + getResults.results[0].category);
+        print("Tipo " + getResults.results[0].type);
+        print("Dificultad " + getResults.results[0].difficulty);
+        print("Atención Pregunta! " + getResults.results[0].question);
     }
 }
